9f42cc4bd40208e1689353ce3e426d5ebe599e7d
DevStuki <devstuki@gmail.com>
2018-11-05 17:04:44 +0900

    Chapter 5 Done!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from "react";
 import MyComponent from "./MyComponent";
 import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <EventPractice />;
+    return (
+      <div>
+        {console.log(this)}
+        <ScrollBox ref={ref => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>Bottom</button>
+      </div>
+    );
   }
 }
 

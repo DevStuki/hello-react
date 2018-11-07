1acbe5a07f2412fe45ecb9f4a40f8271c3256c0f
DevStuki <devstuki@gmail.com>
2018-11-07 18:05:57 +0900

    Chapter 6 Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Component, Fragment } from "react";
-import MyComponent from "./MyComponent";
-import EventPractice from "./EventPractice";
-import ValidationSample from "./ValidationSample";
-import ScrollBox from "./ScrollBox";
+import React, { Component } from "react";
+// import MyComponent from "./MyComponent";
+// import EventPractice from "./EventPractice";
+// import ValidationSample from "./ValidationSample";
+// import ScrollBox from "./ScrollBox";
+// import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 import "./App.css";
 
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: "#000000"
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
     return (
       <div>
-        {console.log(this)}
-        <ScrollBox ref={ref => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>Bottom</button>
+        <button onClick={this.handleClick}>Random Color</button>
+        <LifeCycleSample color={this.state.color} />
       </div>
     );
   }
